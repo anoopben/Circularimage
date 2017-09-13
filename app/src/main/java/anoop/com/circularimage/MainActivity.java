44bda2fea486db1afd0d49a11ee942934661efa3
anoopben <anoopbenny1@gmail.com>
2017-09-14 00:15:40 +0530

    circular image code

---
+++ app/src/main/java/anoop/com/circularimage/MainActivity.java
@@ -0,0 +1,19 @@
+package anoop.com.circularimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        de.hdodenhof.circleimageview.CircleImageView firstimage = (de.hdodenhof.circleimageview.CircleImageView) findViewById(R.id.profile_image);
+
+        int imageResource = getResources().getIdentifier("@drawable/tiger",null,this.getPackageName());
+        firstimage.setImageResource(imageResource);
+
+    }
+}
